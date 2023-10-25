442e616c2c9c545f03140e1a3cad00cec03005ba
grandhivyshnavi <vyshnavigrandhi14@gmail.com>
2023-10-25 18:16:06 +0530

    first commit

---
+++ src/components/EditStudent.js
@@ -0,0 +1,45 @@
+import { useParams } from "react-router-dom";
+import StudentForm from "./StudentForm";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
+function EditStudent() {
+    const { id } = useParams();
+    const [data, setData] = useState({ name: "", email: "", rollNo: "" });
+    const [newData, setNewData] = useState([]);
+    useEffect(() => {
+        Axios.get("https://crud-deployment-backend-k1yb.onrender.com/studentRoute/update-student/" + id)
+            .then((res) => {
+                if (res.status === 200) {
+                    const { name, email, rollNo } = res.data;
+                    setData({ name, email, rollNo });
+                }
+
+                else
+                    Promise.reject();
+            })
+            .catch((err) => alert(err))
+    }, [id]);
+
+    const getState = (childData) => {
+        setNewData(childData);
+    }
+    const handleSubmit = () => {
+        const data = { name: newData[0], email: newData[1], rollNo: newData[2] }
+        Axios.put("http://localhost:4000/studentRoute/update-student/" + id, data)
+            .then((res) => {
+                if (res.status === 200)
+                    alert("Recorded updated successfully");
+                else
+                    Promise.reject();
+            })
+            .catch((err) => alert(err))
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <StudentForm getState={getState} nameValue={data.name} emailValue={data.email} rollNoValue={data.rollNO} >Update Student</StudentForm>
+        </form>
+    )
+}
+export default EditStudent;
+
